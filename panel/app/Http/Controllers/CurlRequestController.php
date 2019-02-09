e5fa4ad4607905ea62b2f793f8d4a405ad6fe44f
Jubair Mizan <jubairmizan@gmail.com>
2019-02-09 21:05:16 +0600

    Create CurlRequestController

---
+++ panel/app/Http/Controllers/CurlRequestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Helpers/CurlRequest;
+
+class CurlRequestController extends Controller
+{
+    public function __construct(){
+    }
+    
+    public function AppModule(Request $request){
+      
+    }
+}
